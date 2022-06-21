dda559fc495f9d672a662d68ec68f70696c3b29b
Elnur <104248864+ElnurGenCeLi@users.noreply.github.com>
2022-06-21 22:07:49 +0400

    update main.py

--- main.py
+++ main.py
@@ -104,8 +104,8 @@ def set_master(update, context):
 
     game.set_master(update.message.from_user.id)
 
-    show_word_btn = InlineKeyboardButton("👻Sözə bax", callback_data='show_word')
-    change_word_btn = InlineKeyboardButton("♻️Sözü dəyiş", callback_data='change_word')
+    show_word_btn = InlineKeyboardButton("Sözə baxmaq 🔎", callback_data='show_word')
+    change_word_btn = InlineKeyboardButton("Növbəti Söz ➡️", callback_data='change_word')
 
     keyboard = [[show_word_btn], [change_word_btn]]
     reply_markup = InlineKeyboardMarkup(keyboard)
